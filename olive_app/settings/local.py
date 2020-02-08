0e138a41cd01be049107f4d74a70d6abea8f6bdb
ldonjibson <ajibolaolayanju@gmail.com>
2020-02-08 18:48:38 +0100

    Adjust registration, password

--- olive_app/settings/local.py
+++ olive_app/settings/local.py
@@ -10,8 +10,6 @@ INSTALLED_APPS += [
 
 MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware', ]
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
 DEBUG_TOOLBAR_CONFIG = {
     'JQUERY_URL': '',
 }
@@ -30,8 +28,27 @@ DATABASES = {
     }
 }
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = '127.0.0.1'
 EMAIL_PORT = 1025
 EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
-EMAIL_USE_TLS = False
+# EMAIL_USE_TLS = False
+DEFAULT_FROM_EMAIL = 'vtu@mailing.vtubusinessportal.com' 
+
+THEME_CONTACT_EMAIL = DEFAULT_FROM_EMAIL #os.getenv('THEME_CONTACT_EMAIL')
+ADMINS = (('Admin', THEME_CONTACT_EMAIL),)
+MANAGERS = ADMINS
+EMAIL_SUBJECT_PREFIX = ''
+SEND_BROKEN_LINK_EMAILS=True
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+# EMAIL_PORT = os.getenv('EMAIL_PORT')
+# EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+# EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+# EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS')
+# EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL')
+# SERVER_EMAIL = os.getenv('SERVER_EMAIL')
+# DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
+# EMAIL_HOST = os.getenv('EMAIL_HOST')
--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 """olive_app URL Configuration
 """
 from django.conf import settings
-from django.urls import path
+from django.urls import path, reverse_lazy
 from django.contrib.auth import views as auth_views
 from django.views.generic import TemplateView
 from django.contrib.auth.views import LoginView
@@ -24,9 +24,10 @@ urlpatterns = [
   path('activations_sent', TemplateView.as_view(template_name='users/account_activation_sent.html'), name='account_activation_sent'),
 
   path('change-password', change_password, name='change_password'),
-  path('password_reset', auth_views.PasswordResetView.as_view(template_name= 'users/password_reset_form.html'), name='password_reset'),
+  path('password_reset', auth_views.PasswordResetView.as_view(template_name= 'users/password_reset_form.html', email_template_name='users/password_reset_email.html', \
+    subject_template_name='users/password_reset_subject.txt', success_url = reverse_lazy('users:password_reset_done')), name='password_reset'),
   path('password_reset/done', auth_views.PasswordResetDoneView.as_view(template_name= 'users/password_reset_done.html'), name='password_reset_done'),
-  path('reset/<uidb64>/<token>/reset',auth_views.PasswordResetConfirmView.as_view(template_name= 'users/password_reset_confirm.html'), name='password_reset_confirm'),
+  path('reset/<uidb64>/<token>/reset',auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
   path('reset/done', auth_views.PasswordResetCompleteView.as_view(template_name= 'users/password_reset_complete.html'), name='password_reset_complete'),
 
 ]
